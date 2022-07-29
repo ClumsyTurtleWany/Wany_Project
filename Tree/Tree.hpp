d3f9479cd6cad8005f52f2262cb58128b901b1b7
ClumsyTurtleWany <kdw41424142@gmail.com>
2022-07-29 19:05:30 +0900

    tree 클래스를 bst 클래스로 변경

--- Tree/Tree.hpp
+++ Tree/Tree.hpp
@@ -6,44 +6,49 @@ namespace TREE
 	// node
 	// depth: parent size
 	// height: child depth
-	template <typename T>
+	template <typename K, typename T>
 	struct node
 	{
+		K key = K();
 		T data = T();
 		int depth = 0;
-		int index = 0;
 		int height = 0;
-		node<T>* parent = nullptr;
-		node<T>* child[2] = { nullptr, };
+		node<K, T>* parent = nullptr;
+		node<K, T>* child[2] = { nullptr, };
 	};
 
-	template <typename T>
-	class Tree
+	template <typename K, typename T>
+	class BST
 	{
+		using NODE = node<K, T>;
 	private:
-		node<T>* root = nullptr;
+		NODE* root = nullptr;
 		int degree = 0;
-		int size = 0;
+		int cnt = 0;
+		int minKey = 0;
+		int maxKey = 0;
 
+		
+		
 	public:
-		Tree() {};
-		~Tree() {};
+		BST() {};
+		~BST() {};
 
 		class iterator
 		{
 		private:
-			node<T>* cur;
+			NODE* cur;
 		public:
-			iterator(node<T>* _data = nullptr) : cur(_data) {};
+			iterator(NODE* _data = nullptr) : cur(_data) {};
 			~iterator() {};
 
 			iterator& operator++()
 			{
 				if (cur->child[1] != nullptr)
 				{
-					if (cur->data < cur->child[1]->data)
+					if (cur->key < cur->child[1]->key)
 					{
-						node<T>* temp = cur->child[1];
+						NODE* temp = cur->child[1];
 						while (temp->child[0] != nullptr)
 						{
 							temp = temp->child[0];
@@ -56,15 +61,15 @@ namespace TREE
 
 				if (cur->child[1] == nullptr)
 				{
-					if (cur->data < cur->parent->data)
+					if (cur->key < cur->parent->key)
 					{
 						cur = cur->parent;
 						return *this;
 					}
 					else
 					{
-						node<T>* temp = cur->parent;
-						while (temp->data < cur->data)
+						NODE* temp = cur->parent;
+						while (temp->key < cur->key)
 						{
 							if (temp->parent != nullptr)
 							{
@@ -93,24 +98,29 @@ namespace TREE
 			{
 				if (cur->child[0] != nullptr)
 				{
-					if (cur->child[0]->data < cur->data)
+					if (cur->child[0]->key < cur->key)
 					{
-						cur = cur->child[0];
+						NODE* temp = cur->child[0];
+						while (temp->child[1] != nullptr)
+						{
+							temp = temp->child[1];
+						}
+						cur = temp;
 						return *this;
 					}
 				}
 
 				if (cur->child[0] == nullptr)
 				{
-					if (cur->parent->data < cur->data)
+					if (cur->parent->key < cur->key)
 					{
 						cur = cur->parent;
 						return *this;
 					}
 					else
 					{
-						node<T>* temp = cur->parent;
-						while (cur->data < temp->data)
+						NODE* temp = cur->parent;
+						while (cur->key < temp->key)
 						{
 							if (temp->parent != nullptr)
 							{
@@ -136,7 +146,7 @@ namespace TREE
 				return temp;
 			}
 
-			node<T>* operator&()
+			NODE* operator&()
 			{
 				return cur;
 			}
@@ -158,47 +168,33 @@ namespace TREE
 			}
 		};
 
-		void insert(T _data, node<T>* _parent = nullptr);
-		iterator find(T _data, node<T>* _parent = nullptr)
-		{
-			if (root == nullptr)
-			{
-				return iterator(nullptr);
-			}
-			else
-			{
-				if (_parent == nullptr)
-				{
-					_parent = root;
-				}
+		void insert(NODE* _node, NODE* _parent = nullptr);
+		void insert(K _key, T _data);
 
-				if (_data < _parent->data)
-				{
-					return find(_data, _parent->child[0]);
-				}
-				else if (_data > _parent->data)
-				{
-					return find(_data, _parent->child[1]);
-				}
-				else
-				{
-					return iterator(_parent);
-				}
-			}
-		}
+		node<K, T>* find(K _key);
+		node<K, T>* find(K _key, NODE* _parent);
+		
+		iterator begin() { return iterator(find(minKey)); };
+		iterator end() { return iterator(find(maxKey)); };
 
-		void erase(TREE::Tree<T>::iterator _iter);
+		bool swap(NODE* _first, NODE* _second);
+
+		int size() { return cnt; };
+
+		void erase(iterator _iter);
+		void erase(K _key);
 
 	};
 
-	template<typename T>
-	inline void Tree<T>::insert(T _data, node<T>* _parent)
+	template<typename K, typename T>
+	inline void BST<K, T>::insert(NODE* _node, NODE* _parent)
 	{
 		if (root == nullptr)
 		{
-			root = new node<T>;
-			root->data = _data;
-			size++;
+			root = _node;
+			minKey = _node->key;
+			maxKey = _node->key;
+			cnt++;
 		}
 		else
 		{
@@ -207,46 +203,220 @@ namespace TREE
 				_parent = root;
 			}
 
-			if (_data < _parent->data)
+			if (_node->key < _parent->key)
 			{
 				if (_parent->child[0] == nullptr)
 				{
-					_parent->child[0] = new node<T>;
-					_parent->child[0]->parent = _parent;
-					_parent->child[0]->data = _data;
-					_parent->child[0]->depth = _parent->depth + 1;
-					size++;
+					_node->parent = _parent;
+					_node->depth = _parent->depth + 1;
+					_parent->child[0] = _node;
+					minKey = minKey < _node->key ? minKey : _node->key;
+					maxKey = maxKey < _node->key ? _node->key : maxKey;
+					cnt++;
 				}
 				else
 				{
-					insert(_data, _parent->child[0]);
+					insert(_node, _parent->child[0]);
 				}
 			}
-			else if(_data > _parent->data)
+			else if(_node->key > _parent->key)
 			{
 				if (_parent->child[1] == nullptr)
 				{
-					_parent->child[1] = new node<T>;
-					_parent->child[1]->parent = _parent;
-					_parent->child[1]->data = _data;
-					_parent->child[1]->depth = _parent->depth + 1;
-					size++;
+					_node->parent = _parent;
+					_node->depth = _parent->depth + 1;
+					_parent->child[1] = _node;
+					minKey = minKey < _node->key ? minKey : _node->key;
+					maxKey = maxKey < _node->key ? _node->key : maxKey;
+					cnt++;
 				}
 				else
 				{
-					insert(_data, _parent->child[1]);
+					insert(_node, _parent->child[1]);
 				}
 			}
 			else
 			{
-				_parent->data = _data;
+				_parent->key = _node->key;
+				_parent->data = _node->data;
+				minKey = minKey < _node->key ? minKey : _node->key;
+				maxKey = maxKey < _node->key ? _node->key : maxKey;
 			}
 		}
 	}
 
-	template<typename T>
-	inline void Tree<T>::erase(TREE::Tree<T>::iterator _iter)
+	template<typename K, typename T>
+	inline void BST<K, T>::insert(K _key, T _data)
+	{
+		NODE* newNode = new NODE;
+		newNode->key = _key;
+		newNode->data = _data;
+		insert(newNode, root);
+	}
+
+	template<typename K, typename T>
+	inline node<K, T>* BST<K, T>::find(K _key, NODE* _parent)
+	{
+		if (root == nullptr)
+		{
+			return nullptr;
+		}
+		else
+		{
+			if (_parent == nullptr)
+			{
+				_parent = root;
+			}
+
+			if (_key < _parent->key)
+			{
+				return find(_key, _parent->child[0]);
+			}
+			else if (_key > _parent->key)
+			{
+				return find(_key, _parent->child[1]);
+			}
+			else
+			{
+				return _parent;
+			}
+		}
+	}
+	
+	template<typename K, typename T>
+	inline node<K, T>* BST<K, T>::find(K _key)
+	{
+		if (root == nullptr)
+		{
+			return nullptr;
+		}
+		else
+		{
+			NODE* temp = root;
+			if (_key < temp->key)
+			{
+				return find(_key, temp->child[0]);
+			}
+			else if (_key > temp->key)
+			{
+				return find(_key, temp->child[1]);
+			}
+			else
+			{
+				return temp;
+			}
+		}
+	}
+
+	template<typename K, typename T>
+	inline bool BST<K, T>::swap(NODE* _first, NODE* _second)
+	{
+		if (_first != nullptr && _second != nullptr)
+		{
+			
+			NODE temp = *_first;
+			NODE* firstParent = _first->parent;
+			NODE* firstChildLeft = _first->child[0];
+			NODE* firstChildRight = _first->child[1];
+
+			NODE* secondParent = _second->parent;
+			NODE* secondChildLeft = _second->child[0];
+			NODE* secondChildRight = _second->child[1];
+			
+			if (firstParent->child[0] == _first)
+			{
+				firstParent->child[0] = _second;
+			}
+			else if (firstParent->child[1] == _first)
+			{
+				firstParent->child[1] = _second;
+			}
+
+			if (secondParent->child[0] == _second)
+			{
+				secondParent->child[0] = _first;
+			}
+			else if (secondParent->child[1] == _second)
+			{
+				secondParent->child[1] = _first;
+			}
+
+			if (secondChildLeft == nullptr)
+			{
+
+			}
+
+			/*template < class K, class T>
+			void	TTable<K, T>::erase(K k)
+			{
+				m_pBstRoot = removeBst(k, m_pBstRoot);
+			}
+			template < class K, class T>
+			TBstNode<K, T>* TTable<K, T>::removeBst(K k, TBstNode<K, T>*t)
+			{
+				TBstNode<K, T>* temp;
+				if (t == NULL)
+				{
+					return NULL;
+				}
+				else if (k < t->m_key)
+				{
+					t->m_pChild[0] = removeBst(k, t->m_pChild[0]);
+				}
+				else if (k > t->m_key)
+				{
+					t->m_pChild[1] = removeBst(k, t->m_pChild[1]);
+				}
+				else if (t->m_pChild[0] && t->m_pChild[1])
+				{
+					temp = findMin(t->m_pChild[1]);
+					t->m_key = temp->m_key;
+					t->m_pChild[1] = removeBst(t->m_key, t->m_pChild[1]);
+				}
+				else
+				{
+					temp = t;
+					if (t->m_pChild[0] == NULL)
+						t = t->m_pChild[1];
+					else if (t->m_pChild[1] == NULL)
+						t = t->m_pChild[0];
+					delete temp;
+				}
+				return t;
+			}*/
+
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	template<typename K, typename T>
+	inline void BST<K, T>::erase(TREE::BST<K, T>::iterator _iter)
 	{
 		
+	}
+
+	template<typename K, typename T>
+	inline void BST<K, T>::erase(K _key)
+	{
+		// target->child[0]->child[1] 과 target 교환
+		// 혹은 target->child[1]->child[0]과 교환
+		// child[0] == nullptr && child[1] == nullptr 인 노드까지 target이 도달했을 떄
+		// target 삭제.
+
+		NODE* target = find(_key);
+		if (target != nullptr)
+		{
+			int a = 0;
+			swap(target, root);
+
+			
+
+
+
+		}
 	}
 }
--- Tree/main.cpp
+++ Tree/main.cpp
@@ -1,27 +1,43 @@
 #include "main.h"
-#include <set>
+#include <string>
 
 void main()
 {
-	TREE::Tree<int> binTree;
+	TREE::BST<int, std::string> binTree;
 
-	binTree.insert(7);
-	binTree.insert(3);
-	binTree.insert(2);
-	binTree.insert(5);
-	binTree.insert(9);
-	binTree.insert(8);
-	binTree.insert(10);
-	binTree.insert(6);
+	/*binTree.insert(25, "1");
+	binTree.insert(7, "2");
+	binTree.insert(3, "3");
+	binTree.insert(2, "4");
+	binTree.insert(5, "5");
+	binTree.insert(9, "6");
+	binTree.insert(8, "7");
+	binTree.insert(10, "8");
+	binTree.insert(6, "9");
+	binTree.insert(16, "10");
+	binTree.insert(13, "11");
+	binTree.insert(1, "12");
+	binTree.insert(22, "13");
+	binTree.insert(32, "14");
+	binTree.insert(56, "15");
+	binTree.erase(8);*/
 
-	auto it = binTree.find(2);
-	it++;
-	it++;
-	it++;
-	it++;
-	it++;
-	it++;
-	it++;
-	it++;
-	it++;
+	binTree.insert(4, "root");
+	binTree.insert(2, "root-child[0]");
+	binTree.insert(3, "root-child[0]-child[1]");
+
+	//     4
+	//    /
+	//   2
+	//    \
+	//     3
+
+	binTree.erase(3);
+
+	std::cout << "Size: " << binTree.size() << std::endl;
+	for (auto it = binTree.begin(); it != binTree.end(); it++)
+	{
+		std::cout << "Key: " << (&it)->key << ", Data: " << (&it)->data << std::endl;
+	}
+	
 }
