789203bbb6aad989137b883c4feb74e13cbf3551
ClumsyTurtleWany <kdw41424142@gmail.com>
2022-09-08 19:00:31 +0900

    update

--- CoreSpaceDivisionTest_0/Sample.h
+++ CoreSpaceDivisionTest_0/Sample.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "GameCore.hpp"
+#include "SpaceDivision.hpp"
+#include "QuadTree.hpp"
+#include "Octree.hpp"
 
 //C:\Users\kgca4108\Documents\Visual Studio 2019\My Exported Templates\CoreTestTemplate_0.zip
 class Sample : public GameCore
--- CoreSpaceDivisionTest_0/SpaceDivision.hpp
+++ CoreSpaceDivisionTest_0/SpaceDivision.hpp
@@ -1,7 +1,5 @@
 #pragma once
 #include "Node.hpp"
-#include "NPC.hpp"
-#include "Obstacle.hpp"
 
 class SpaceDivision
 {
