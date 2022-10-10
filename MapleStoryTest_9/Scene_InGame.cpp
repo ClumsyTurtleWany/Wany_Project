96999ac0e38095c4b375b3cd9a04a6e985b4cf17
ClumsyTurtleWany <kdw41424142@gmail.com>
2022-10-10 21:18:00 +0900

    1. update test

--- MapleStoryTest_9/Scene_InGame.cpp
+++ MapleStoryTest_9/Scene_InGame.cpp
@@ -232,8 +232,6 @@ bool Scene_InGame::render()
 	wstrCameraPos += L", ";
 	wstrCameraPos += std::to_wstring(static_cast<int>((renderCamera->getRect().LT.y + renderCamera->getRect().RB.y) / 2));
 	DXWriter::getInstance()->draw(0, 75, wstrCameraPos);*/
-	// 됨?
-	// 다시 안되냐
 
 	for (auto it : uiList)
 	{
